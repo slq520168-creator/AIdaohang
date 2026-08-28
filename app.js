910c62cb910f95f6afd19b52a7a0dbd661640cc6
slq520168-creator <slq520168@gmail.com>
2026-08-28 21:51:27 +0800

    load through more50

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function match(t0,c){
 async function load(){
   applyChrome();
   const files=['data/tools.json','data/packs.json','data/more.json'];
-  for(let i=2;i<=40;i++) files.push('data/more'+i+'.json');
+  for(let i=2;i<=50;i++) files.push('data/more'+i+'.json');
   const arrs=await Promise.all(files.map(f=>fetch(f).then(r=>r.ok?r.json():[]).catch(()=>[])));
   const seen=new Set(); tools=[];
   for(const x of arrs.flat()){if(!x||!x.name||seen.has(x.name))continue;seen.add(x.name);tools.push(x)}
