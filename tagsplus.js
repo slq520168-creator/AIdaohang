7483b9b43b46c8e0cfffe2e10a08d2c8f959cce5
slq520168-creator <slq520168@gmail.com>
2026-08-30 17:00:34 +0800

    add matrix republish schedulers free oss

--- tagsplus.js
+++ tagsplus.js
@@ -1,6 +1,6 @@
 (function(){
   if(typeof TAGS==='undefined') return;
-  [['问卷','Survey'],['二手','Used'],['跑腿','Errand'],['维修','Repair'],['美容','Salon']].forEach(function(p){
+  [['问卷','Survey'],['二手','Used'],['跑腿','Errand'],['维修','Repair'],['美容','Salon'],['矩阵','Matrix'],['转发','Repost'],['定时','Schedule']].forEach(function(p){
     if(!TAGS.some(function(t){return t[0]===p[0]})) TAGS.push(p);
     if(typeof TAG_EN!=='undefined') TAG_EN[p[0]]=p[1];
   });
