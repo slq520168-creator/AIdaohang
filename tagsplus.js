05f15490d2ba3317578bef595294118253c65299
slq520168-creator <slq520168@gmail.com>
2026-08-30 15:34:18 +0800

    tagsplus and tools v=175

---
+++ tagsplus.js
@@ -0,0 +1,7 @@
+(function(){
+  if(typeof TAGS==='undefined') return;
+  [['问卷','Survey'],['二手','Used'],['跑腿','Errand'],['维修','Repair'],['美容','Salon']].forEach(function(p){
+    if(!TAGS.some(function(t){return t[0]===p[0]})) TAGS.push(p);
+    if(typeof TAG_EN!=='undefined') TAG_EN[p[0]]=p[1];
+  });
+})();
