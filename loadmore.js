b13867ce7c595dd72a92a23d3024408e9abcd6fd
slq520168-creator <slq520168@gmail.com>
2026-08-31 09:39:16 +0000

    Add public cloud-phone, fingerprint-browser and device-control tools.

--- loadmore.js
+++ loadmore.js
@@ -16,7 +16,7 @@
   function grab(n){
     fetch('data/more'+n+'.json').then(function(r){return r.ok?r.json():[];}).then(add).catch(function(){});
   }
-  function run(){for(var i=184;i<=196;i++) grab(i);}
+  function run(){for(var i=184;i<=197;i++) grab(i);}
   if(document.readyState==='complete') setTimeout(run,400);
   else window.addEventListener('load',function(){setTimeout(run,400);});
 })();
